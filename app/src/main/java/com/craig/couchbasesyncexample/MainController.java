166824a49c01e76577ccf9d6f94c1bcbfded0d08
Clifton Craig <clifton.craig@ge.com>
2017-01-21 15:29:43 -0800

    Adds initial couchbase integration.

--- app/src/main/java/com/craig/couchbasesyncexample/MainController.java
+++ app/src/main/java/com/craig/couchbasesyncexample/MainController.java
@@ -2,14 +2,47 @@ package com.craig.couchbasesyncexample;
 
 import android.util.Log;
 
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Manager;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Clifton Craig on 1/21/17.
  * Copyright GE 1/21/17
  */
 public class MainController {
     private static final String TAG = MainController.class.getName();
+    private Manager cbManager;
+    private Database database;
+    int counter = 0;
 
     public void onGo() {
+        Log.d(TAG, "Begin Go button click handler.");
+        final Map<String, Object> document = new HashMap<String, Object>() {{
+            put("name", "Cliff");
+            put("age", "40");
+            put("count", counter++);
+        }};
+        final Document couchbaseDoc = getDatabase().createDocument();
+        try { couchbaseDoc.putProperties(document); }
+        catch (CouchbaseLiteException e) { e.printStackTrace(); }
+        Log.d(TAG, "Created doc with ID " + couchbaseDoc.getId());
         Log.d(TAG, "Go button click was handled.");
+    }
+
+    public void setCBManager(Manager cbmanager) {
+        this.cbManager = cbmanager;
+    }
+
+    public void setDatabase(Database database) {
+        this.database = database;
+    }
+
+    public Database getDatabase() {
+        return database;
     }
 }
--- app/src/main/java/com/craig/couchbasesyncexample/SimpleDependencyInjector.java
+++ app/src/main/java/com/craig/couchbasesyncexample/SimpleDependencyInjector.java
@@ -1,16 +1,64 @@
 package com.craig.couchbasesyncexample;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.android.AndroidContext;
+
+import java.io.IOException;
+
 /**
  * Created by Clifton Craig on 1/21/17.
  * Copyright GE 1/21/17
  */
 public class SimpleDependencyInjector implements DependencyInjector {
+
+    private final SingleThreadWorker asyncWorker;
+
+    public SimpleDependencyInjector() {
+        asyncWorker = new SingleThreadWorker();
+    }
+
     @Override
     public void inject(Object rootObject) {
         injectMainActivity((MainActivity)rootObject);
     }
 
-    private void injectMainActivity(MainActivity mainActivity) {
-        mainActivity.setAsyncWorker(new SingleThreadWorker());
+    private void injectMainActivity(final MainActivity mainActivity) {
+        asyncWorker.doAsync(new Runnable() {
+            public void run() {
+                doInjection(mainActivity);
+            }
+        });
+    }
+
+    private void doInjection(MainActivity mainActivity) {
+        mainActivity.setAsyncWorker(asyncWorker);
+        final Manager manager = getManagerForContext(mainActivity);
+        mainActivity.getMainController().setCBManager(manager);
+        mainActivity.getMainController().setDatabase(getDatabaseForManager(manager));
+    }
+
+    private Database getDatabaseForManager(Manager manager) {
+        final Database database;
+        try { database = manager.getDatabase(getDatabaseName()); }
+        catch (CouchbaseLiteException e) { throw new RuntimeException("Could not create database.", e); }
+        return database;
+    }
+
+    @NonNull
+    private String getDatabaseName() {
+        return "todo";
+    }
+
+    @NonNull
+    private Manager getManagerForContext(Context context) {
+        final Manager manager;
+        try { manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS); }
+        catch (IOException e) { throw new RuntimeException("Cannot setup Couchbase manager.", e); }
+        return manager;
     }
 }
